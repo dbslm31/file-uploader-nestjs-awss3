e6f44d605cd36e4657cb26945438c696152b44b0
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2024-10-30 19:24:26 +0100

    Basic config and AWS config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //CORS config
+  app.enableCors({
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  //Server config
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
